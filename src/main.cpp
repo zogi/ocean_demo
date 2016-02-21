966884d3a75c7458cf14af81d1dfb37002fbe20b
Zoltan Gilian <zoltan.gilian@gmail.com>
2016-02-21 17:19:05 +0100

    rename window title

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    rendering::main_window main_window(640, 480, "demo", 4);
+    rendering::main_window main_window(640, 480, "ocean demo", 4);
 
     gpu::compute compute(main_window.get_graphics_context());
 
