9724a233eb355809a3893cfa65cae885e2bfbec9
Zoltan Gilian <zoltan.gilian@gmail.com>
2016-02-22 21:14:13 +0100

    make main_window itself handle its events

--- include/main_window.h
+++ include/main_window.h
@@ -13,26 +13,21 @@ public:
     main_window(const main_window&) = delete;
     main_window& operator=(const main_window&) = delete;
 
-    void process_events() { window.process_events(event_handler); }
+    void process_events();
     void swap_frame();
     void attach_camera(scene::camera& camera);
     run_state get_run_state() const { return run_state; }
     os::window::size get_size() const { return window.get_size(); }
     auto get_graphics_context() const { return window.get_graphics_context(); }
 
+    void handle_event(const os::event& event);
+
 private:
-    friend class event_handler;
-    struct event_handler {
-        event_handler(main_window& parent) : parent(parent) {}
-        void handle_event(const os::event& event);
-        main_window& parent;
-    };
     void handle_quit_event();
     void handle_resize_event(const os::window::size& size);
     void handle_keyboard_event(const os::keyboard_event& event);
 
     os::window window;
-    event_handler event_handler;
     rendering::multisample_framebuffer framebuffer;
     scene::camera_controller main_camera_controller;
     run_state run_state;
--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1,7 +1,7 @@
 #include <main_window.h>
 
 main_window::main_window(int width, int height, const char *title, int num_framebuffer_samples)
-    : window(width, height, title), event_handler(*this),
+    : window(width, height, title),
       framebuffer(width, height, num_framebuffer_samples),
       run_state(RUN_STATE_RUNNING)
 {
@@ -13,23 +13,28 @@ void main_window::attach_camera(scene::camera& camera)
     main_camera_controller.set_viewport_size(window.get_size());
 }
 
+void main_window::process_events()
+{
+    window.process_events<main_window&>(*this);
+}
+
 void main_window::swap_frame()
 {
     framebuffer.resolve_to_backbuffer();
     window.swap_frame();
 }
 
-void main_window::event_handler::handle_event(const os::event& event)
+void main_window::handle_event(const os::event& event)
 {
     if (event.is_quit_event()) {
-        parent.handle_quit_event();
+        handle_quit_event();
     } else if (event.is_window_resize_event()) {
-        parent.handle_resize_event(event.get_window_size());
+        handle_resize_event(event.get_window_size());
     } else if (event.is_keyboard_event()) {
-        parent.handle_keyboard_event(event.get_keyboard_event());
+        handle_keyboard_event(event.get_keyboard_event());
     }
 
-    parent.main_camera_controller.handle_event(event);
+    main_camera_controller.handle_event(event);
 }
 
 void main_window::handle_quit_event()
