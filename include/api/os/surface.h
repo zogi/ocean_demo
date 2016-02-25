7907a84b7ae60caf95c1812aaacc9dcc6aeeafa7
Zoltan Gilian <zoltan.gilian@gmail.com>
2016-02-25 23:10:11 +0100

    add move ctor and move assignment to surface

--- include/api/os/surface.h
+++ include/api/os/surface.h
@@ -11,7 +11,9 @@ public:
     surface(SDL_Surface *api_surface) : api_surface(api_surface) {}
     ~surface() { SDL_FreeSurface(api_surface); }
     surface(const surface&) = delete;
+    surface(surface&& rhs) = default;
     surface& operator=(const surface&) = delete;
+    surface& operator=(surface&&) = default;
     util::extent get_extent() const { return { api_surface->w, api_surface->h }; }
     void *get_pixels() const { return api_surface->pixels; }
 private:
