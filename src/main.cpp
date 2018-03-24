e5f6526a06dc7ced80d0c0fea9c5e4987f79d7ee
Zoltan Gilian <zoltan.gilian@gmail.com>
2018-03-24 20:35:20 +0100

    add clarifying comments

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char *argv[])
 
     ocean::surface_params ocean_params;
     ocean_params.fft_size = math::ivec2(1024, 1024);
-    ocean_params.tile_size_logical = math::vec3(100, 100, 100);
-    ocean_params.tile_size_physical = math::vec3(200, 200, 200);
+    ocean_params.tile_size_logical = math::vec3(100, 100, 100);  // For rendering.
+    ocean_params.tile_size_physical = math::vec3(200, 200, 200); // For heightmap generation.
     ocean_params.amplitude = 1.0e-3;
     ocean_params.wavelength_low_threshold = math::real(0.7);
     ocean_params.set_wind_vector(math::vec2(15, 0));
